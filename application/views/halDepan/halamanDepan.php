e8ce1d4b2db66c400032338a71b013fce17ef9bd
Zefanya <vendradevendra@gmail.com>
2020-10-02 16:43:23 +0700

    halut baru
    modal login yang belum bisa

--- application/views/halDepan/halamanDepan.php
+++ application/views/halDepan/halamanDepan.php
@@ -5,67 +5,68 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>Sistem Informasi Akademik Seminari Garum</title>
-    <link rel="stylesheet" href="<?php echo base_url(). 'assets/induko/bootstrap/css/bootstrap.min.css'?>">
+    <link rel="stylesheet" href="<?php echo base_url().'assets/induko/bootstrap/css/bootstrap.min.css'?>">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kaushan+Script">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700">
-    <link rel="stylesheet" href="<?php echo base_url(). 'assets/induko/fonts/font-awesome.min.css'?>">
+    <link rel="stylesheet" href="<?php echo base_url().'assets/fonts/font-awesome.min.css'?>">
 </head>
 
 <body id="page-top">
     <nav class="navbar navbar-dark navbar-expand-lg fixed-top bg-dark" id="mainNav">
-        <div class="container">
+        <div class="container"><a class="navbar-brand" href="#page-top">SEMINARI GARUM</a><button data-toggle="collapse" data-target="#navbarResponsive" class="navbar-toggler navbar-toggler-right" type="button" data-toogle="collapse" aria-controls="navbarResponsive" aria-expanded="false"
+                aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="nav navbar-nav ml-auto text-uppercase">
-                  <li class="nav-item" role="presentation"><a class="navbar-brand" href="#page-top">Beranda</a><button data-toggle="collapse" data-target="#navbarResponsive" class="navbar-toggler navbar-toggler-right" type="button" data-toogle="collapse" aria-controls="navbarResponsive" aria-expanded="false"
-                aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
+                    <li class="nav-item" role="presentation"><a class="nav-link js-scroll-trigger" href="#">Beranda</a></li>
                     <li class="nav-item" role="presentation"><a class="nav-link js-scroll-trigger" href="#tentang">Tentang</a></li>
                     <li class="nav-item" role="presentation"><a class="nav-link js-scroll-trigger" href="#contact">Hubungi Kami</a></li>
-
                     <li class="nav-item" ><a href="" class="nav-link" data-toggle="modal" data-target="#modalLoginForm">Login</a></li>
                     <div class="modal fade" id="modalLoginForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"aria-hidden="true">
-                      <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                          <div class="modal-header text-center">
-                            <h4 class="modal-title w-100 font-weight-bold">Sign in</h4>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                              <span aria-hidden="true">&times;</span>
-                            </button>
-                          </div>
-                          <div class="modal-body mx-3">
-                            <div class="md-form mb-5">
-                              <i class="fas fa-user prefix grey-text"></i>
-                              <input type="text" id="form34" class="form-control validate">
-                              <label data-error="wrong" data-success="right" for="form34">Username</label>
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header text-center">
+                                    <h4 class="modal-title w-100 font-weight-bold">Sign in</h4>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body mx-3">
+                                    <div class="md-form mb-5">
+                                        <i class="fas fa-user prefix grey-text"></i>
+                                        <input type="text" id="form3" class="form-control validate">
+                                        <label data-error="wrong" data-success="right" for="defaultForm-email">Username</label>
+                                    </div>
+                                    <div class="md-form mb-4">
+                                        <i class="fas fa-lock prefix grey-text"></i>
+                                        <input type="password" id="defaultForm-pass" class="form-control validate">
+                                        <label data-error="wrong" data-success="right" for="defaultForm-pass">Password</label>
+                                    </div>
+                                </div>
+                                <div class="modal-footer d-flex justify-content-center">
+                                    <button class="btn btn-default">Login</button>
+                                </div>
                             </div>
-                            <div class="md-form mb-4">
-                              <i class="fas fa-lock prefix grey-text"></i>
-                              <input type="password" id="defaultForm-pass" class="form-control validate">
-                              <label data-error="wrong" data-success="right" for="defaultForm-pass">Password</label>
-                            </div>
-                             </div>
-                             <div class="modal-footer d-flex justify-content-center">
-                              <button class="btn btn-default">Login</button>
-                            </div>
-                          </div>
                         </div>
-                      </div>
                     </div>
-
-                    <li class="nav-item"><a class="nav-link" href="#"><img src="<?php echo base_url ('assets/induko/img/search_icon.png')?>" alt="#" /></a></li>
                 </ul>
             </div>
         </div>
     </nav>
+
     <header class="masthead" style="background-image:url(<?php echo base_url('assets/adminBaru/img/sgar-1.jpg')?>);">
         <div class="container">
             <div class="intro-text">
-                <div class="intro-lead-in"><span>SELAMAT DATANG!</span></div>
-                <h1><span>Sistem Informasi Akademik Seminari Garum</span></h1>
-        </div>
+                <div class="intro-heading text-uppercase"><span>SELAMAT DATANG</span></div>
+                <h2><span>Sistem Informasi Akademik Seminari Garum</span></h2>
+                <h4>Seminari Menengah  St. Vincentius A Paulo<br>Jl. Merdeka Timur 4-6 Garum, Blitar</h4>
+              <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" role="button" href="#tentang">Tell me more</a>
+           </div>
+       </div>
     </header>
 
+    
     <section id="tentang">
         <div class="container">
             <div class="row">
@@ -78,7 +79,7 @@
                 <div class="col-lg-12">
                     <ul class="list-group timeline">
                         <li class="list-group-item">
-                            <div class="timeline-image"><img class="rounded-circle img-fluid" src="<?php echo base_url('assets/induko/img/about/1.jpg')?>"></div>
+                            <div class="timeline-image"><img class="rounded-circle img-fluid" src="<?php echo base_url ('assets/induko/img/about/1.jpg')?>"></div>
                             <div class="timeline-panel">
                                 <div class="timeline-heading">
                                     <h4>2009-2011</h4>
@@ -90,7 +91,7 @@
                             </div>
                         </li>
                         <li class="list-group-item timeline-inverted">
-                            <div class="timeline-image"><img class="rounded-circle img-fluid" src="<?php echo base_url('assets/induko/img/about/2.jpg')?>"></div>
+                            <div class="timeline-image"><img class="rounded-circle img-fluid" src="<?php echo base_url ('assets/induko/img/about/2.jpg')?>"></div>
                             <div class="timeline-panel">
                                 <div class="timeline-heading">
                                     <h4>March 2011</h4>
@@ -102,7 +103,7 @@
                             </div>
                         </li>
                         <li class="list-group-item">
-                            <div class="timeline-image"><img class="rounded-circle img-fluid" src="<?php echo base_url('assets/induko/img/about/3.jpg')?>"></div>
+                            <div class="timeline-image"><img class="rounded-circle img-fluid" src="<?php echo base_url ('assets/induko/img/about/3.jpg')?>"></div>
                             <div class="timeline-panel">
                                 <div class="timeline-heading">
                                     <h4>December 2012</h4>
@@ -114,7 +115,7 @@
                             </div>
                         </li>
                         <li class="list-group-item timeline-inverted">
-                            <div class="timeline-image"><img class="rounded-circle img-fluid" src="<?php echo base_url('assets/induko/img/about/4.jpg')?>"></div>
+                            <div class="timeline-image"><img class="rounded-circle img-fluid" src="<?php echo base_url ('assets/induko/img/about/4.jpg')?>"></div>
                             <div class="timeline-panel">
                                 <div class="timeline-heading">
                                     <h4>July 2014</h4>
@@ -140,17 +141,31 @@
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 text-center">
-                    <h2 class="text-uppercase section-heading">HUBUNGI KAMI</h2>
+                    <h2 class="text-uppercase section-heading">Hubungi Kami</h2>
                     <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
                 </div>
             </div>
         </div>
     </section>
-
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4"><span class="copyright">Seminari Garum</span></div>
+                <div class="col-md-4">
+                    <ul class="list-inline social-buttons">
+                        <li class="list-inline-item"><a href="#"><i class="fa fa-instagram "></i></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="fa fa-facebook"></i></a></li>
+                        <li class="list-inline-item"><a href="#"><i class="fa fa-youtube"></i></a></li>
+                    </ul>
+                </div>   
+            </div>
+        </div>
+    </footer>
+    
     <script src="<?php echo base_url('assets/induko/js/jquery.min.js')?>"></script>
     <script src="<?php echo base_url('assets/induko/bootstrap/js/bootstrap.min.js')?>"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
-    <script src="<?php echo base_url('assets/js/agency.js')?>"></script>
+    <script src="<?php echo base_url('assets/induko/js/agency.js')?>"></script>
 </body>
 
 </html>
