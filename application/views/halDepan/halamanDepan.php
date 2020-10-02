c79e9c9f48cc374615b30fa0bb4933574aad5e31
Zefanya <vendradevendra@gmail.com>
2020-10-02 13:48:40 +0700

    halutBaru

--- application/views/halDepan/halamanDepan.php
+++ application/views/halDepan/halamanDepan.php
@@ -1,125 +1,156 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-	<!-- Basic -->
-   <meta charset="utf-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <!-- Mobile Metas -->
-   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-   <!-- Site Metas -->
-   <title>Sistem Informasi Akademik Seminari Garum</title>
-   <meta name="keywords" content="">
-   <meta name="description" content="">
-   <meta name="author" content="">
-   <!-- site icon -->
-   <link rel="icon" href="<?php echo base_url('assets/induko/images/fevicon.png') ?>" type="image/png" />
-   <!-- Bootstrap core CSS -->
-   <link href="<?php echo base_url('assets/induko/css/bootstrap.css')?>" rel="stylesheet">
-   <!-- FontAwesome Icons core CSS -->
-   <link href="<?php echo base_url('assets/induko/css/font-awesome.min.css')?>" rel="stylesheet">
-   <!-- Custom animate styles for this template -->
-   <link href="<?php echo base_url('assets/induko/css/animate.css')?>" rel="stylesheet">
-   <!-- Custom styles for this template -->
-   <link href="<?php echo base_url('assets/induko/style.css')?>" rel="stylesheet">
-   <!-- Responsive styles for this template -->
-   <link href="<?php echo base_url('assets/induko/css/responsive.css')?>" rel="stylesheet">
-   <!-- Colors for this template -->
-   <link href="<?php echo base_url('assets/induko/css/colors.css')?>" rel="stylesheet">
-   <!-- light box gallery -->
-   <link href="<?php echo base_url('assets/induko/css/ekko-lightbox.css')?>" rel="stylesheet">
-  
-   <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-   <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-   
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Sistem Informasi Akademik Seminari Garum</title>
+    <link rel="stylesheet" href="<?php echo base_url(). 'assets/induko/bootstrap/css/bootstrap.min.css'?>">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kaushan+Script">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700">
+    <link rel="stylesheet" href="<?php echo base_url(). 'assets/induko/fonts/font-awesome.min.css'?>">
 </head>
-<body id="home_page" class="home_page">
-      <!-- header -->
-      <header class="header">
 
-        <div class="header_top_section">
-           <div class="container">
-              <div class="row">
-               <div class="col-lg-3">
-                  <div class="full">
-                     <div class="logo">
-                        <a href="index.html" alt="#" ></a>
-                     </div>
-                  </div>
-               </div>
-               <div class="col-lg-9 site_information">
-                  <div class="full">
-                     <div class="main_menu">
-                        <nav class="navbar navbar-inverse navbar-toggleable-md">
-                           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#cloapediamenu" aria-controls="cloapediamenu" aria-expanded="false" aria-label="Toggle navigation">
-                           <span class="float-left">Menu</span>
-                           <span class="float-right"><i class="fa fa-bars"></i> <i class="fa fa-close"></i></span>
-                           </button>
-                           <div class="collapse navbar-collapse justify-content-md-center" id="cloapediamenu">
-                              <ul class="navbar-nav">
-                                 <li class="nav-item active">
-                                    <a class="nav-link" href="index.html">Beranda</a>
-                                 </li>
-                                 <li class="nav-item">
-                                    <a class="nav-link color-aqua-hover" href="about.html">Tentang</a>
-                                 </li>
-                                <!--  <li class="nav-item">
-                                    <a class="nav-link color-aqua-hover" href="coaching.html">Coaching</a>
-                                 </li>
-                                 <li class="nav-item">
-                                    <a class="nav-link color-grey-hover" href="time.html">Time</a>
-                                 </li> -->
-                                 <li class="nav-item">
-                                    <a class="nav-link color-grey-hover" href="contact.html">Hubungi Kami</a>
-                                 </li>
-                                 <li class="nav-item">
-                                    <a class="nav-link color-grey-hover" href="contact.html">Login</a>
-                                 </li>
-                              </ul>
-                              <ul class="navbar-nav">
-                                 <li class="nav-item">
-                                    <a class="nav-link" href="#"><img src="<?php echo base_url('assets/induko/images/search_icon.png')?>" alt="#" /></a>
-                                 </li>
-                              </ul>
-                           </div>
-                        </nav>
-                     </div>
-                  </div>
-               </div>
-            </div>
-           </div>
-        </div>
+<body id="page-top">
+    <nav class="navbar navbar-dark navbar-expand-lg fixed-top bg-dark" id="mainNav">
+        <div class="container">
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="nav navbar-nav ml-auto text-uppercase">
+                  <li class="nav-item" role="presentation"><a class="navbar-brand" href="#page-top">Beranda</a><button data-toggle="collapse" data-target="#navbarResponsive" class="navbar-toggler navbar-toggler-right" type="button" data-toogle="collapse" aria-controls="navbarResponsive" aria-expanded="false"
+                aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
+                    <li class="nav-item" role="presentation"><a class="nav-link js-scroll-trigger" href="#tentang">Tentang</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link js-scroll-trigger" href="#contact">Hubungi Kami</a></li>
 
-      </header>
-      <!-- end header -->
-      <!-- section -->
-      <section class="main_full banner_section_top">
-        <div class="container-fluid">
-          <div class="row">
-             <div class="full">
-                  <div class="slider_banner">
-                    <img class="img-responsive" src="<?php echo base_url('assets/adminBaru/img/sgar-1.jpg')?>" alt="#" />
-                      <div class="slide_cont">
-                        <div class="slider_cont_inner">
-                          <h3>Welcome To Our  Web Coaching</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit</p>
-                        <a class="blue_bt" href="#">Read More</a>
+                    <li class="nav-item" ><a href="" class="nav-link" data-toggle="modal" data-target="#modalLoginForm">Login</a></li>
+                    <div class="modal fade" id="modalLoginForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"aria-hidden="true">
+                      <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                          <div class="modal-header text-center">
+                            <h4 class="modal-title w-100 font-weight-bold">Sign in</h4>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                              <span aria-hidden="true">&times;</span>
+                            </button>
+                          </div>
+                          <div class="modal-body mx-3">
+                            <div class="md-form mb-5">
+                              <i class="fas fa-user prefix grey-text"></i>
+                              <input type="text" id="form34" class="form-control validate">
+                              <label data-error="wrong" data-success="right" for="form34">Username</label>
+                            </div>
+                            <div class="md-form mb-4">
+                              <i class="fas fa-lock prefix grey-text"></i>
+                              <input type="password" id="defaultForm-pass" class="form-control validate">
+                              <label data-error="wrong" data-success="right" for="defaultForm-pass">Password</label>
+                            </div>
+                             </div>
+                             <div class="modal-footer d-flex justify-content-center">
+                              <button class="btn btn-default">Login</button>
+                            </div>
+                          </div>
                         </div>
                       </div>
-                  </div>
-                </div>
-          </div>
+                    </div>
+
+                    <li class="nav-item"><a class="nav-link" href="#"><img src="<?php echo base_url ('assets/induko/img/search_icon.png')?>" alt="#" /></a></li>
+                </ul>
+            </div>
         </div>
-      </section>
-      <!-- end section -->
-      <!-- end footer -->
-      <!-- Core JavaScript
-         ================================================== -->
-      <script src="<?php echo base_url('assets/induko/js/jquery.min.js')?>"></script>
-      <script src="<?php echo base_url('assets/induko/js/tether.min.js')?>"></script>
-      <script src="<?php echo base_url('assets/induko/js/bootstrap.min.js')?>"></script>
-      <script src="<?php echo base_url('assets/induko/js/parallax.js')?>"></script>
-      <script src="<?php echo base_url('assets/induko/js/animate.js')?>"></script>
-      <script src="<?php echo base_url('assets/induko/js/ekko-lightbox.js')?>"></script>
-      <script src="<?php echo base_url('assets/induko/js/custom.js')?>"></script>
-   </body>
+    </nav>
+    <header class="masthead" style="background-image:url(<?php echo base_url('assets/adminBaru/img/sgar-1.jpg')?>);">
+        <div class="container">
+            <div class="intro-text">
+                <div class="intro-lead-in"><span>SELAMAT DATANG!</span></div>
+                <h1><span>Sistem Informasi Akademik Seminari Garum</span></h1>
+        </div>
+    </header>
+
+    <section id="tentang">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="text-uppercase">Tentang Kami</h2>
+                    <h3 class="text-muted section-subheading">Lorem ipsum dolor sit amet consectetur.</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <ul class="list-group timeline">
+                        <li class="list-group-item">
+                            <div class="timeline-image"><img class="rounded-circle img-fluid" src="<?php echo base_url('assets/induko/img/about/1.jpg')?>"></div>
+                            <div class="timeline-panel">
+                                <div class="timeline-heading">
+                                    <h4>2009-2011</h4>
+                                    <h4 class="subheading">Our Humble Beginnings</h4>
+                                </div>
+                                <div class="timeline-body">
+                                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="list-group-item timeline-inverted">
+                            <div class="timeline-image"><img class="rounded-circle img-fluid" src="<?php echo base_url('assets/induko/img/about/2.jpg')?>"></div>
+                            <div class="timeline-panel">
+                                <div class="timeline-heading">
+                                    <h4>March 2011</h4>
+                                    <h4 class="subheading">An Agency is Born</h4>
+                                </div>
+                                <div class="timeline-body">
+                                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="list-group-item">
+                            <div class="timeline-image"><img class="rounded-circle img-fluid" src="<?php echo base_url('assets/induko/img/about/3.jpg')?>"></div>
+                            <div class="timeline-panel">
+                                <div class="timeline-heading">
+                                    <h4>December 2012</h4>
+                                    <h4 class="subheading">Transition to Full Service</h4>
+                                </div>
+                                <div class="timeline-body">
+                                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="list-group-item timeline-inverted">
+                            <div class="timeline-image"><img class="rounded-circle img-fluid" src="<?php echo base_url('assets/induko/img/about/4.jpg')?>"></div>
+                            <div class="timeline-panel">
+                                <div class="timeline-heading">
+                                    <h4>July 2014</h4>
+                                    <h4 class="subheading">Phase Two Expansion</h4>
+                                </div>
+                                <div class="timeline-body">
+                                    <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="list-group-item timeline-inverted">
+                            <div class="timeline-image">
+                                <h4>Be Part<br>&nbsp;Of Our<br>&nbsp;Story!</h4>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <section id="contact" style="background-image:url(<?php echo base_url('assets/induko/img/map-image.png')?>);">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="text-uppercase section-heading">HUBUNGI KAMI</h2>
+                    <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <script src="<?php echo base_url('assets/induko/js/jquery.min.js')?>"></script>
+    <script src="<?php echo base_url('assets/induko/bootstrap/js/bootstrap.min.js')?>"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+    <script src="<?php echo base_url('assets/js/agency.js')?>"></script>
+</body>
+
 </html>
