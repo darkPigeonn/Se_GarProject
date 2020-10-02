e6bbc25429a3a17488c7ce01d2137c4ce64511ff
darkPigeonn <50091059+darkPigeonn@users.noreply.github.com>
2020-10-02 16:48:08 +0700

    modal
    hoi

--- application/views/halDepan/halamanDepan.php
+++ application/views/halDepan/halamanDepan.php
@@ -23,33 +23,7 @@
                     <li class="nav-item" role="presentation"><a class="nav-link js-scroll-trigger" href="#tentang">Tentang</a></li>
                     <li class="nav-item" role="presentation"><a class="nav-link js-scroll-trigger" href="#contact">Hubungi Kami</a></li>
                     <li class="nav-item" ><a href="" class="nav-link" data-toggle="modal" data-target="#modalLoginForm">Login</a></li>
-                    <div class="modal fade" id="modalLoginForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"aria-hidden="true">
-                        <div class="modal-dialog" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header text-center">
-                                    <h4 class="modal-title w-100 font-weight-bold">Sign in</h4>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>
-                                <div class="modal-body mx-3">
-                                    <div class="md-form mb-5">
-                                        <i class="fas fa-user prefix grey-text"></i>
-                                        <input type="text" id="form3" class="form-control validate">
-                                        <label data-error="wrong" data-success="right" for="defaultForm-email">Username</label>
-                                    </div>
-                                    <div class="md-form mb-4">
-                                        <i class="fas fa-lock prefix grey-text"></i>
-                                        <input type="password" id="defaultForm-pass" class="form-control validate">
-                                        <label data-error="wrong" data-success="right" for="defaultForm-pass">Password</label>
-                                    </div>
-                                </div>
-                                <div class="modal-footer d-flex justify-content-center">
-                                    <button class="btn btn-default">Login</button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                   
                 </ul>
             </div>
         </div>
@@ -162,6 +136,34 @@
         </div>
     </footer>
     
+<!-- modal -->
+     <div class="modal fade" id="modalLoginForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header text-center">
+                                    <h4 class="modal-title w-100 font-weight-bold">Sign in</h4>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body mx-3">
+                                    <div class="md-form mb-5">
+                                        <i class="fas fa-user prefix grey-text"></i>
+                                        <input type="text" id="form3" class="form-control validate">
+                                        <label data-error="wrong" data-success="right" for="defaultForm-email">Username</label>
+                                    </div>
+                                    <div class="md-form mb-4">
+                                        <i class="fas fa-lock prefix grey-text"></i>
+                                        <input type="password" id="defaultForm-pass" class="form-control validate">
+                                        <label data-error="wrong" data-success="right" for="defaultForm-pass">Password</label>
+                                    </div>
+                                </div>
+                                <div class="modal-footer d-flex justify-content-center">
+                                    <button class="btn btn-default">Login</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
     <script src="<?php echo base_url('assets/induko/js/jquery.min.js')?>"></script>
     <script src="<?php echo base_url('assets/induko/bootstrap/js/bootstrap.min.js')?>"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
